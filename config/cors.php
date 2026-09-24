28e218135af302f92219dfb54ed926490fd6e798
Matheus <nmmatheus@hotmail.com>
2026-09-23 23:21:49 -0300

    fix: habilita sanctum stateful na api e completa config de cors

--- config/cors.php
+++ config/cors.php
@@ -2,6 +2,8 @@
 
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'allowed_methods' => ['*'],
     'allowed_origins' => ['http://localhost:5173'],
+    'allowed_headers' => ['*'],
     'supports_credentials' => true,
 ];
